d6eaf5aa19fc7db4ec365a87e21c1e38ace2e094
hementt <hemantchauhan7988@gmail.com>
2026-05-11 12:49:50 +0530

    Time: 1 ms (28.80%) | Memory: 42.4 MB (7.63%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,5 +1,7 @@
 class Solution {
+    Integer dp[][];
     public int integerBreak(int n) {
+        dp = new Integer[n+1][n+1];
         return solve(n, n, 1);
     }
     public int solve(int n, int target ,int i){
@@ -7,9 +9,11 @@ class Solution {
         if(target == 0) return 1;
         if(i >=  n || target < 0) return 0;
 
+        if(dp[i][target] != null) return dp[i][target];
+
         int include = i * solve(n, target - i, i);
         int exclude = solve(n, target, i+1);
 
-        return Math.max(include, exclude);
+        return dp[i][target] = Math.max(include, exclude);
     }
 }
