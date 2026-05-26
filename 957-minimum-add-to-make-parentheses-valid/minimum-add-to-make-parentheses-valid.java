008114db5969e64e7238224a5c65346c72aaf952
hementt <hemantchauhan7988@gmail.com>
2026-05-26 14:06:28 +0530

    Time: 3 ms (4.40%) | Memory: 43.1 MB (21.11%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int res = 0;
+
+        Stack <Character> stack = new Stack<>();
+        for(int i =0; i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push('(');
+            }else{
+                if(stack.isEmpty()){
+                    res++;
+                    continue;
+                }
+                char peek = stack.peek();
+                if(ch == ')' && peek == '(' || ch == ']' && peek == '[' || ch == '}' && peek == '{'){
+                    stack.pop();
+                }else{
+                    res++;
+                }
+            }
+        }
+        return res + stack.size();
+    }
+}
