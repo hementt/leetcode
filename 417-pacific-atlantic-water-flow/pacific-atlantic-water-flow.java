3fd9c1ac3ac16fbfb7e4c664824d423b65dfc887
hementt <hemantchauhan7988@gmail.com>
2026-07-18 18:49:33 +0530

    Time: 6 ms (38.30%) | Memory: 47.2 MB (92.16%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,103 +1,77 @@
 class Solution {
-    class Pair{
+    class Pair {
         int row;
         int col;
 
-        Pair(int row, int col){
+        Pair(int row, int col) {
             this.row = row;
             this.col = col;
         }
     }
-    public boolean isValid(int newRow, int newCol, int totalRow, int totalCol){
-        if(newRow >= 0 && newRow< totalRow && newCol>=0 && newCol < totalCol){
-            return true;
-        }
-        return false;
+
+    int[] rowDirection = {1, -1, 0, 0};
+    int[] colDirection = {0, 0, 1, -1};
+
+    public boolean isValid(int row, int col, int n, int m) {
+        return row >= 0 && row < n && col >= 0 && col < m;
     }
 
-    int[] rowDirection= {1,-1,0,0};
-    int[] colDirection = {0,0,1,-1}; 
-    boolean pacific[][];
-    boolean atlantic[][];
-    
+    public void bfs(int[][] heights, int i, int j, boolean[][] ocean) {
+        int n = heights.length;
+        int m = heights[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, j));
+        ocean[i][j] = true;
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+
+            int row = curr.row;
+            int col = curr.col;
+
+            for (int k = 0; k < 4; k++) {
+                int newRow = row + rowDirection[k];
+                int newCol = col + colDirection[k];
+
+                if (isValid(newRow, newCol, n, m)
+                        && !ocean[newRow][newCol]
+                        && heights[newRow][newCol] >= heights[row][col]) {
+
+                    ocean[newRow][newCol] = true;
+                    q.add(new Pair(newRow, newCol));
+                }
+            }
+        }
+    }
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int n = heights.length;
         int m = heights[0].length;
 
-        pacific = new boolean[n][m];
-        atlantic = new boolean[n][m];
+        boolean[][] pacific = new boolean[n][m];
+        boolean[][] atlantic = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            bfs(heights, i, 0, pacific);
+            bfs(heights, i, m - 1, atlantic);
+        }
+
+        for (int j = 0; j < m; j++) {
+            bfs(heights, 0, j, pacific);
+            bfs(heights, n - 1, j, atlantic);
+        }
 
         List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i =0; i<n;i++){
-            bfs1(heights, i, 0);
-            bfs2(heights, i, m-1);
-        }
-        for(int i =0;i<m;i++){
-            bfs1(heights, 0, i);
-            bfs2(heights, n-1, i);
-        }
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(pacific[i][j] && atlantic[i][j]){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(i);
-                    list.add(j);
-                    ans.add(list);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.add(Arrays.asList(i, j));
                 }
             }
         }
+
         return ans;
-
-    }public void bfs1(int[][] heights, int i, int j){
-        int n = heights.length;
-        int m = heights[0].length;
-        boolean isVis[][] = new boolean[n][m];
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
-        isVis[i][j] = true;
-
-        while(!q.isEmpty()){
-            Pair curr = q.poll();
-            int row = curr.row;
-            int col = curr.col;
-
-            pacific[row][col] = true;
-            for(int k =0; k<4;k++){
-                int newRow = row + rowDirection[k];
-                int newCol = col + colDirection[k];
-
-                if(isValid(newRow, newCol, n, m) && heights[newRow][newCol] >= heights[row][col] && !isVis[newRow][newCol]){
-                    q.add(new Pair(newRow, newCol));
-                    isVis[newRow][newCol] = true;
-                }
-            }
-        }
-    }public void bfs2(int[][] heights, int i, int j){
-        int n = heights.length;
-        int m = heights[0].length;
-        boolean isVis[][] = new boolean[n][m];
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
-        isVis[i][j] = true;
-
-        while(!q.isEmpty()){
-            Pair curr = q.poll();
-            int row = curr.row;
-            int col = curr.col;
-
-            atlantic[row][col] = true;
-            for(int k =0; k<4;k++){
-                int newRow = row + rowDirection[k];
-                int newCol = col + colDirection[k];
-
-                if(isValid(newRow, newCol, n, m) && heights[newRow][newCol] >= heights[row][col] && !isVis[newRow][newCol]){
-                    q.add(new Pair(newRow, newCol));
-                    isVis[newRow][newCol] = true;
-                }
-            }
-        }
     }
 }
