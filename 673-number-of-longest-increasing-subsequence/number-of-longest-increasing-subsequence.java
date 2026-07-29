6e64a4672a673660ac77802f049330f86bddf86a
hementt <hemantchauhan7988@gmail.com>
2026-07-29 15:28:59 +0530

    Time: 23 ms (20.75%) | Memory: 45.4 MB (75.44%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,15 +1,17 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n+1];
-        int max = 0;
-        int count[] = new int[n+1];
+
+        int dp[] = new int[n];
+        int count[] = new int[n];
         Arrays.fill(count, 1);
         Arrays.fill(dp, 1);
 
+        int max = 1;
 
         for(int i =0; i<n;i++){
-            for(int j = i - 1;j>=0; j--){
+            for(int j =0; j<i;j++){
+
                 if(nums[i] > nums[j]){
                     if(dp[i] < dp[j] + 1){
                         dp[i] = dp[j] + 1;
@@ -17,24 +19,17 @@ class Solution {
                     }else if(dp[i] == dp[j] + 1){
                         count[i] += count[j];
                     }
-
                 }
-
+                max = Math.max(dp[i] , max);
             }
-            max = Math.max(dp[i], max);
         }
-
-
-        
         int res = 0;
-        
 
         for(int i =0; i<n;i++){
-            if(dp[i] == max){
+            if(max == dp[i]){
                 res += count[i];
             }
         }
-
         return res;
     }
 }
